839b4131f62c8468636f6ca948c92e1df2198b93
Rautish Kumar <rautishz@gmail.com>
2024-08-02 20:39:49 +0530

    release 1.0

--- src/services/API.jsx
+++ src/services/API.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ishpay.com",
+  baseURL: "https://ishpay-backend-098e4fc9cb44.herokuapp.com",
 });
 
 API.interceptors.request.use(
